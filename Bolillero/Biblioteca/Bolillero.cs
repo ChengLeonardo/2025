27063c8c794091c4afeb76ee98e80ab30d0d308b
ChengLeonardo <leonardo.chenget12de1@gmail.com>
2025-04-10 01:09:07 -0300

    mvp

---
+++ Bolillero/Biblioteca/Bolillero.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca;
+
+public class Bolillero : IBolillero
+{
+    public List<Bolilla> Bolillas {get;set;} = new List<Bolilla>();
+
+    public Bolillero(int cantidadBolillasBolillero)
+    {
+        GenerarBolillas(cantidadBolillasBolillero);
+    }
+
+    public void GenerarBolillas(int cantidadBolillas)
+    {
+        Bolillas.Clear();
+        for(int i = 0; i < cantidadBolillas; i++)
+        {
+            Bolilla bolilla = new Bolilla(i);
+            Bolillas.Add(bolilla);
+        }
+    }
+
+    public List<Bolilla> GeneradorAzar(int cantidadBolillas)
+    {
+        int limite = Bolillas.Max(bolilla => bolilla.numeroBolilla);
+        List<Bolilla> BolillasSacadas = new();
+        Random rnd = new();
+        for(int i = 1; i <= cantidadBolillas; i++){
+            Bolilla bolillaSacada = new Bolilla(rnd.Next(0, limite));
+            while(BolillasSacadas.Any(bolilla => bolilla.numeroBolilla == bolillaSacada.numeroBolilla)){
+                bolillaSacada = new Bolilla(rnd.Next(0, limite));
+            }
+            BolillasSacadas.Add(bolillaSacada);
+            Bolillas.Remove(bolillaSacada);
+        }
+
+        return BolillasSacadas;
+    }
+
+    public bool CompararRespuesta(List<Bolilla> bolillasSacadas, List<Bolilla> jugada)
+    {
+        bool resultado = true;
+        if(!bolillasSacadas.SequenceEqual(jugada)){
+            return false;
+        }
+        return resultado;
+    }
+
+    public bool Jugar(List<int> jugada)
+    {
+        List<Bolilla> bolillas = new List<Bolilla>();
+        foreach(int numero in jugada)
+        {
+            bolillas.Add(new Bolilla(numero));
+        }
+        var lista = GeneradorAzar(jugada.Count());
+        Console.WriteLine("lista girada: " + lista);
+        bool resultado = CompararRespuesta(lista, bolillas);
+        return resultado;
+    }
+}
---
+++ Bolillero/Biblioteca/BolilleroTest.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca;
+
+public class BolilleroTest : IBolillero
+{
+     public List<Bolilla> Bolillas {get;set;} = new List<Bolilla>();
+
+    public BolilleroTest(int cantidadBolillasBolillero)
+    {
+        GenerarBolillas(cantidadBolillasBolillero);
+    }
+
+    public void GenerarBolillas(int cantidadBolillas)
+    {
+        Bolillas.Clear();
+        for(int i = 0; i < cantidadBolillas; i++)
+        {
+            Bolilla bolilla = new Bolilla(i);
+            Bolillas.Add(bolilla);
+        }
+    }
+
+    public List<Bolilla> GeneradorAzar(int cantidadBolillas)
+    {
+        List<Bolilla> BolillasSacadas = Bolillas.Select(Bolilla => Bolilla).Take(cantidadBolillas).ToList();
+        Bolillas = Bolillas.Except(BolillasSacadas).ToList();
+        return BolillasSacadas;
+    }
+
+    public bool CompararRespuesta(List<Bolilla> bolillasSacadas, List<Bolilla> jugada)
+    {
+        bool resultado = true;
+        if(!bolillasSacadas.SequenceEqual(jugada)){
+            resultado = false;
+        }
+
+        return resultado;
+    }
+
+    public bool Jugar(List<int> jugada)
+    {
+        List<Bolilla> bolillas = new List<Bolilla>();
+        foreach(int numero in jugada)
+        {
+            bolillas.Add(new Bolilla(numero));
+        }
+        var lista = GeneradorAzar(jugada.Count());
+
+        bool resultado = CompararRespuesta(lista, bolillas);
+        return resultado;
+    }
+
+    
+}
